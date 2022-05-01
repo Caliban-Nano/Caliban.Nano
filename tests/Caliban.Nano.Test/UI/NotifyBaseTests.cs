fc27aa33299c1ff913da02a57f851a306ad7e978
Christian Uhsat <christian@uhsat.de>
2022-05-01 17:16:27 +0200

    Added NotifyBase unit test

---
+++ tests/Caliban.Nano.Test/UI/NotifyBaseTests.cs
@@ -0,0 +1,24 @@
+﻿using Caliban.Nano.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Caliban.Nano.Test.UI
+{
+    [TestClass]
+    public class NotifyBaseTests : NotifyBase
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            PropertyChanged += (sender, e) =>
+            {
+                Assert.AreEqual(e.PropertyName, "Test");
+            };
+        }
+
+        [TestMethod]
+        public void NotifyPropertyChangedTest()
+        {
+            NotifyPropertyChanged("Test");
+        }
+    }
+}
