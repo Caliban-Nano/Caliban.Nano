c3d3b006f3bd6a02dcb1df8030432a2bcefde4cd
Christian Uhsat <christian@uhsat.de>
2022-05-03 00:01:52 +0200

    Refactored EventLogger as mixin

--- src/Caliban.Nano/Events/EventLogger.cs
+++ src/Caliban.Nano/Events/EventLogger.cs
@@ -1,25 +1,30 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Caliban.Nano.Contracts;
 
-namespace Caliban.Nano.Events
+namespace Caliban.Nano.Events.EventLogger
 {
     /// <summary>
-    /// A basic event logger add-on.
+    /// An event logger mixin.
     /// </summary>
-    public abstract class EventLogger
+    public static class EventLogger
     {
-        /// <summary>
-        /// Used event aggregator.
-        /// </summary>
-        public IEventAggregator? Events { get; init; }
-
         /// <summary>
         /// Raises a log event for a specified log message.
         /// </summary>
+        /// <param name="logger">The logger instance.</param>
         /// <param name="message">The log message.</param>
-        public void Raise([NotNull] string message)
+        public static void Raise(this ILogger logger, [NotNull] string message)
         {
-            Events?.Publish(new LogEvent(message));
+            var events = IoC.Get<IEventAggregator>();
+            
+            if (events is not null)
+            {
+                events.Publish(new LogEvent(message));
+            }
+            else
+            {
+                Log.Intern("IEventAggregator could not resolved");
+            }
         }
     }
 }
--- tests/Caliban.Nano.Test/Events/EventLoggerTests.cs
+++ tests/Caliban.Nano.Test/Events/EventLoggerTests.cs
@@ -1,5 +1,8 @@
-﻿using Caliban.Nano.Contracts;
+﻿using System;
+using Caliban.Nano.Container;
+using Caliban.Nano.Contracts;
 using Caliban.Nano.Events;
+using Caliban.Nano.Events.EventLogger;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Caliban.Nano.Test.Events
@@ -7,20 +10,41 @@ namespace Caliban.Nano.Test.Events
     [TestClass]
     public class EventLoggerTests
     {
-        private class Logger : EventLogger
+        private class Logger : ILogger
         {
-            public Logger(IEventAggregator events)
-            {
-                Events = events;
-            }
+            public void Info(string message)
+                => throw new NotImplementedException();
+            public void Warn(string message)
+                => throw new NotImplementedException();
+            public void Error(string message)
+                => throw new NotImplementedException();
+            public void Error(string format, params object[] args)
+                => throw new NotImplementedException();
+            public void Error(Exception exception)
+                => throw new NotImplementedException();
+            public void Error(Exception exception, string format, params object[] args)
+                => throw new NotImplementedException();
+        }
+
+        private IContainer? Container { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var events = new EventAggregator();
+
+            Container = new NanoContainer();
+            Container.Register<IEventAggregator>(events);
+
+            IoC.Resolve = Container.Resolve;
         }
 
         [TestMethod]
         public void RaiseTest()
         {
-            var events = new EventAggregator();
-            var logger = new Logger(events);
-
+            var events = IoC.Get<IEventAggregator>();
+            var logger = new Logger();
+            
             events.Subscribe<LogEvent>((LogEvent e) => {
                 Assert.AreEqual(e.Message, "test");
             });
