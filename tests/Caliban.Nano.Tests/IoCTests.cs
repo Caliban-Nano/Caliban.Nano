8cf96c6f6933500e8ba300d2a474b4c5a37917dc
Christian Uhsat <christian@uhsat.de>
2022-05-03 22:58:17 +0200

    Added IoC unit tests

---
+++ tests/Caliban.Nano.Tests/IoCTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Caliban.Nano.Container;
+using Caliban.Nano.Contracts;
+using Caliban.Nano.Test.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Caliban.Nano.Tests
+{
+    [TestClass]
+    public sealed class IoCTests
+    {
+        private IContainer? Container { get; set; }
+
+        private void Initialize()
+        {
+            Container = new NanoContainer();
+
+            IoC.Resolve = Container.Resolve;
+        }
+
+        [TestMethod]
+        public void ExceptionTest()
+        {
+            Assert.ThrowsException<NotImplementedException>(() => IoC.Get<TestClass>());
+        }
+
+        [TestMethod]
+        public void ResolveTest()
+        {
+            Initialize();
+
+            var test = new TestClass();
+
+            Container?.Register<TestClass>(test);
+
+            Assert.AreEqual(IoC.Resolve(typeof(TestClass)), test);
+        }
+
+        [TestMethod]
+        public void GetTest()
+        {
+            Initialize();
+
+            var test = new TestClass();
+
+            Container?.Register<TestClass>(test);
+
+            Assert.AreEqual(IoC.Get<TestClass>(), test);
+        }
+    }
+}
