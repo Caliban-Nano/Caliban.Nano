07d6df7af384a4214e52b30e9cea2d28e9103696
Christian Uhsat <christian@uhsat.de>
2022-05-03 23:47:06 +0200

    Added ViewBinder unit tests

---
+++ tests/Caliban.Nano.Tests/UI/ViewBinderTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+using Caliban.Nano.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Caliban.Nano.Tests.UI
+{
+    [TestClass]
+    public sealed class ViewBinderTests
+    {
+        [TestMethod]
+        public void AddResolverTest()
+        {
+            ViewBinder.Scope.Clear();
+
+            Assert.AreEqual(ViewBinder.Scope.Count, 0);
+
+            ViewBinder.AddResolver<Control>(new ViewBinder.Resolver((_, _) => true));
+
+            Assert.AreEqual(ViewBinder.Scope.Count, 1);
+        }
+
+        [TestMethod]
+        public void BindTest()
+        {
+            Assert.IsTrue(true); // TODO
+        }
+    }
+}
