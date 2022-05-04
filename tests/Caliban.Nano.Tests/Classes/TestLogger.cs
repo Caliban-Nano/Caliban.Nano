e6f4c6cc13a7a229be590638c64f15fcd43c47b3
Christian Uhsat <christian@uhsat.de>
2022-05-04 16:26:56 +0200

    Added test logger class

---
+++ tests/Caliban.Nano.Tests/Classes/TestLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Caliban.Nano.Contracts;
+
+namespace Caliban.Nano.Tests.Classes
+{
+    internal sealed class TestLogger : ILogger
+    {
+        public void Info(string message)
+            => throw new NotImplementedException();
+        public void Warn(string message)
+            => throw new NotImplementedException();
+        public void Error(string message)
+            => throw new NotImplementedException();
+        public void Error(string format, params object[] args)
+            => throw new NotImplementedException();
+        public void Error(Exception exception)
+            => throw new NotImplementedException();
+        public void Error(Exception exception, string format, params object[] args)
+            => throw new NotImplementedException();
+    }
+}
