afb587e5fe4b02ae109ec60b78f77a31dc588e5a
Christian Uhsat <christian@uhsat.de>
2022-05-04 16:26:45 +0200

    Added bootstrap unit tests

---
+++ tests/Caliban.Nano.Tests/BootstrapTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Caliban.Nano.Container;
+using Caliban.Nano.Tests.Classes;
+using Caliban.Nano.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Caliban.Nano.Tests
+{
+    [TestClass]
+    public class BootstrapTests
+    {
+        [TestMethod]
+        public void BootstrapTest()
+        {
+            var bootstrap = new Bootstrap();
+
+            Assert.IsNotNull(bootstrap.Container);
+            Assert.IsInstanceOfType(bootstrap.Container, typeof(NanoContainer));
+
+            Assert.AreEqual(TypeFinder.Sources.Count, 1);
+
+            bootstrap.AddSource(GetType().Assembly);
+
+            Assert.AreEqual(TypeFinder.Sources.Count, 2);
+
+            Assert.ThrowsException<TypeLoadException>(() => IoC.Get<IDependency>());
+
+            bootstrap.Register<IDependency>(new TestClass());
+
+            Assert.IsInstanceOfType(IoC.Get<IDependency>(), typeof(TestClass));
+        }
+    }
+}
