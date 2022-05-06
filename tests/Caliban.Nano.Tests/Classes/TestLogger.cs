38f8c346d2a149928eec252610b9b600b7fdb338
Christian Uhsat <christian@uhsat.de>
2022-05-06 12:55:46 +0200

    Refactored TestLogger

--- tests/Caliban.Nano.Tests/Classes/TestLogger.cs
+++ tests/Caliban.Nano.Tests/Classes/TestLogger.cs
@@ -5,17 +5,11 @@ namespace Caliban.Nano.Tests.Classes
 {
     internal sealed class TestLogger : ILogger
     {
-        public void Info(string message)
-            => throw new NotImplementedException();
-        public void Warn(string message)
-            => throw new NotImplementedException();
-        public void Error(string message)
-            => throw new NotImplementedException();
-        public void Error(string format, params object[] args)
-            => throw new NotImplementedException();
-        public void Error(Exception exception)
-            => throw new NotImplementedException();
-        public void Error(Exception exception, string format, params object[] args)
-            => throw new NotImplementedException();
+        public void Info(string message) { }
+        public void Warn(string message) { }
+        public void Error(string message) { }
+        public void Error(string format, params object[] args) { }
+        public void Error(Exception exception) { }
+        public void Error(Exception exception, string format, params object[] args) { }
     }
 }
