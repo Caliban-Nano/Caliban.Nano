3b5591e672ad36027d0b2c60941c7f663e9eca73
Christian Uhsat <christian@uhsat.de>
2022-05-31 15:30:54 +0200

    Changed sample shutdown

--- samples/Caliban.Nano.Ninject/UI/MainViewModel.cs
+++ samples/Caliban.Nano.Ninject/UI/MainViewModel.cs
@@ -1,20 +1,27 @@
 ﻿using System.Windows;
+using Caliban.Nano.Contracts;
 using Caliban.Nano.Ninject.Data;
 using Caliban.Nano.UI;
 using Ninject;
 
 namespace Caliban.Nano.Ninject.UI
 {
-    public sealed class MainViewModel : ViewModel
+    public sealed class MainViewModel : ViewModel, IWindow
     {
         [Inject]
         public IMessage? Message { get; init; }
+        public string DisplayName => "Ninject Sample";
 
         public MainViewModel()
         {
             MessageBox.Show(Message?.Content ?? "Error");
 
-            Application.Current.Shutdown();
+            Shutdown();
+        }
+
+        public async void Shutdown()
+        {
+            await Close();
         }
     }
 }
