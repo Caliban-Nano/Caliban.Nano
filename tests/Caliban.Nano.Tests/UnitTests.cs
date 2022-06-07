18300747e57634ac926acdb2a542a4bc861191c2
Christian Uhsat <christian@uhsat.de>
2022-06-07 16:46:02 +0200

    Added unit test test

---
+++ tests/Caliban.Nano.Tests/UnitTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Caliban.Nano.Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsNotNull("All Systems Nominal");
+        }
+    }
+}
